d8a86611da0224c9912141b7917b8864b953899d
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2023-06-11 16:42:27 +0200

    Add generic title component for widget

---
+++ src/components/generic/Title.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let label : String;
+</script>
+
+<div>
+    <p class="title">{label}</p>
+</div>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .title {
+        padding: 5px 0 5px 0;
+        background-color: #d8eaea;
+        text-align: center;
+        border-bottom: solid black 1px;
+    }
+</style>
