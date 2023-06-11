77ecc0ad900d5e1944c99a155fd55704f3261d08
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2023-06-11 19:25:26 +0200

    Add attack property to hero class

--- src/data/Hero.ts
+++ src/data/Hero.ts
@@ -1,9 +1,11 @@
 export default class Hero {
     public name: string;
     public level: number;
+    public attack: number; // note that it's damage per seconds (dps)
 
     constructor(name: string, level: number) {
         this.name = name;
         this.level = level;
+        this.attack = level * 5;
     }
 }
