1cbd1fa44303024665dad424c41e2f8cc52e820d
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2023-06-11 16:43:04 +0200

    Use title inside heroes list component

--- src/components/HeroesList.svelte
+++ src/components/HeroesList.svelte
@@ -2,6 +2,7 @@
   import type Hero from "../data/Hero";
   import HeroItemView from "./HeroItemView.svelte";
   import { heroes } from "../store/Stores";
+    import Title from "./generic/Title.svelte";
 
   function setName(event) {
     heroes.update((h) => SetName(h, event));
@@ -13,22 +14,8 @@
   }
 </script>
 
-<div>
-  <p class="title">Heroes</p>
-</div>
+<Title label="Heroes"/>
+
 {#each $heroes as hero, i}
   <HeroItemView {hero} index={i} on:setName={setName} />
 {/each}
-
-<style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  .title {
-    padding: 5px 0 5px 0;
-    background-color: #D8EAEA;
-    text-align: center;
-    border-bottom: solid black 1px;
- }
-</style>
