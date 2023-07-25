4b1fd7fe25561dba69250546d6b3cc99a40d7e7d
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2023-07-25 13:19:54 +0200

    Update upgrade recipe to found which equipment is related to which equipement

--- src/data/UpgradeRecipe.ts
+++ src/data/UpgradeRecipe.ts
@@ -1,22 +1,35 @@
 import type Equipment from "./Equipment";
-import type { SlotType } from "./Equipment";
+import { SlotType } from "./Equipment";
 
 export default class UpgradeRecipe {
-    private static recipes: UpgradeRecipe[] = [];
+    public static recipes: UpgradeRecipe[] = [];
     
     public readonly levelMin: number;
     public readonly levelMax: number;
     public readonly slotType: SlotType;
-    public readonly recipes: [string, number];
+    public readonly recipes: [string, number][];
 
-    constructor(levelMin: number, levelMax: number, slotType: SlotType, recipes: [string, number]) {
+    constructor(levelMin: number, levelMax: number, slotType: SlotType, recipes: [string, number][]) {
         this.levelMin = levelMin;
         this.levelMax = levelMax;
         this.slotType = slotType;
         this.recipes = recipes;
     }
 
-    private contains(equipment: Equipment) : boolean {
-        return this.levelMin <= equipment.levelRequired && equipment.levelRequired <= this.levelMax;
+    private IsFor(equipment: Equipment) : boolean {
+        return (this.levelMin <= equipment.levelRequired && equipment.levelRequired <= this.levelMax) &&
+                equipment.slotType == (this.slotType & equipment.slotType);
     }   
+
+    public static getRecipeFor(equipment: Equipment) : [string, number][] {
+        var recipe: UpgradeRecipe = this.recipes.find(r => r.IsFor(equipment));
+
+        if(recipe == undefined) {
+            throw new Error(`No upgrade recipe found for ${equipment.name} (Lv${equipment.levelRequired}, Slot:${equipment.slotType})`)
+        }
+
+        return recipe.recipes;
+    }
 }
+
+UpgradeRecipe.recipes.push(new UpgradeRecipe(1, 10, SlotType.All, [["Iron", 5]]))
