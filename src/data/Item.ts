70f6341fdc5e80b04dfae88eb21a7f4b07717393
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2023-08-14 22:05:48 +0200

    Add item for level 21 - 30

--- src/data/Item.ts
+++ src/data/Item.ts
@@ -27,6 +27,7 @@ export default class Item extends Loot {
 
     public static readonly upgrade10 = ["Granacier", "Jabacite"]
     public static readonly upgrade20 = ["Primavite", "Patium", "Morvite"]
+    public static readonly upgrade30 = ["Pepitium", "Triarite", "Ronavite"]
 }
 
 Item.items.push(new Item("Jabacite", "jabacite", Loot.golfForLevel(5) / 3, 1));
@@ -35,3 +36,7 @@ Item.items.push(new Item("Granacier", "granacier", Loot.golfForLevel(5) / 5, 2))
 Item.items.push(new Item("Primavite", "primavite", Loot.golfForLevel(15), 1));
 Item.items.push(new Item("Patium", "patium", Loot.golfForLevel(15) / 2.5, 2));
 Item.items.push(new Item("Morvite", "morvite", Loot.golfForLevel(15) / 5, 4));
+
+Item.items.push(new Item("Triarite", "triarite", Loot.golfForLevel(25) / 2, 1));
+Item.items.push(new Item("Ronavite", "ronavite", Loot.golfForLevel(25) / 3, 3));
+Item.items.push(new Item("Pepitium", "pepitium", Loot.golfForLevel(25) / 5, 6));
