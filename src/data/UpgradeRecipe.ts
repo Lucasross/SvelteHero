c482996d71aefb19a4991aa5fe408749d831d001
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2023-08-15 14:24:05 +0200

    Fix upgrade recipe for level 21 - 30

--- src/data/UpgradeRecipe.ts
+++ src/data/UpgradeRecipe.ts
@@ -74,4 +74,4 @@ UpgradeRecipe.recipes.push(new UpgradeRecipe(1, 10, SlotType.All, 5000, [["Grana
 
 UpgradeRecipe.recipes.push(new UpgradeRecipe(11, 20, SlotType.All, 13500, [["Morvite", 3], ["Patium", 1], ["Primavite", 0.5]]))
 
-UpgradeRecipe.recipes.push(new UpgradeRecipe(11, 20, SlotType.All, 24700, [["Pepitium", 5], ["Patium", 1.35], ["Primavite", 0.25]]))
+UpgradeRecipe.recipes.push(new UpgradeRecipe(21, 30, SlotType.All, 24700, [["Pepitium", 5], ["Patium", 1.35], ["Primavite", 0.25]]))
