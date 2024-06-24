06e2445513afabd6a3be07d264a60c853e3a41e8
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2024-06-24 18:33:19 +0200

    Enhance settings view

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -5,7 +5,11 @@
     import { logOut } from "./../services/firebase";
 
     // Firebase database
-    import { signInWithGoogle, saveGame, loadGame } from "../services/firebase.js";
+    import {
+        signInWithGoogle,
+        saveGame,
+        loadGame,
+    } from "../services/firebase.js";
 
     let userID;
 
@@ -24,18 +28,32 @@
 <div>
     <Title label="Settings" />
     <div class="template sign-in">
-        <div>
-            {#if userID != null}
-                <button on:click={loadGame}>Load</button>
-                <button on:click={saveGame}>Save</button>
-            {/if}
-        </div>
+        {#if userID != null}
+            <div>
+                <button on:click={loadGame}>
+                    <i class="fa-solid fa-download"></i>
+                    Load
+                </button>
+            </div>
+            <div>
+                <button on:click={saveGame}>
+                    <i class="fa-solid fa-floppy-disk"></i>
+                    Save
+                </button>
+            </div>
+        {/if}
 
         <div>
             {#if userID == null}
-                <button on:click={signInWithGoogle}>Log In</button>
+                <button on:click={signInWithGoogle}>
+                    <i class="fa-solid fa-right-to-bracket"></i>
+                    Log In
+                </button>
             {:else}
-                <button on:click={logOut}>Log Out</button>
+                <button on:click={logOut}>
+                    <i class="fa-solid fa-right-to-bracket reverse"></i>
+                    Log Out
+                </button>
             {/if}
         </div>
     </div>
@@ -43,6 +61,7 @@
 
 <style>
     .sign-in {
+        padding-top: 8px;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -53,5 +72,8 @@
         border: solid black 1px;
         border-top: 0px;
         background-color: #fdfdfd;
+    }
+    .reverse {
+        transform: scaleX(-1)
     }
 </style>
