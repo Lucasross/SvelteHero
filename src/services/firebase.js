c92f557751c388d5a8ed63b486c07001eec4ba7f
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2024-07-07 14:28:57 +0200

    remove get analytics

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -19,7 +19,6 @@ const firebaseConfig = {
 
 console.log("App initialize firestore")
 initializeApp(firebaseConfig);
-getAnalytics(app);
 
 export const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
